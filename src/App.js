7b3a026d386b7cfd015f9e7d1af9ea18bdfb2ed5
MassMatsu <masa10matsukawa@gmail.com>
2021-02-17 17:23:04 +0900

    setup index.js and html heading on App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>pagination</h1>
+        <div className='underline'></div>
+      </div>
+      <section className='followers'>
+        <div className='container'></div>
+      </section>
+    </main>
+  );
+};
+
+export default App;
