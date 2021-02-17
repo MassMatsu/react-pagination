7143ae8fd22af14d33ff28fc7150c9650db815db
MassMatsu <masa10matsukawa@gmail.com>
2021-02-17 21:21:41 +0900

    completed App.js setting up rendering cards required per page and pagination buttons with its functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFetch } from './useFetch';
 import Follower from './Follower';
 
@@ -6,25 +6,69 @@ const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
 
 const App = () => {
   const { loading, data } = useFetch(url);
+  const [followers, setFollowers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
-  console.log(loading, data);
-  // useEffect(() => {
-  //   if (loading) return;
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[currentPage]);
+  }, [loading, currentPage, data]);
 
-  // }, [loading]);
+  const handlePrev = () => {
+    setCurrentPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+  const handleNext = () => {
+    setCurrentPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
 
   return (
     <main>
       <div className='section-title'>
-        <h1>pagination</h1>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
         <div className='underline'></div>
       </div>
       <section className='followers'>
         <div className='container'>
-          {data.map((follower) => {
-            return <Follower key={follower.id} {...follower} />;
-          })}
+          {followers &&
+            followers.map((follower) => {
+              return <Follower key={follower.id} {...follower} />;
+            })}
         </div>
+        {!loading && (
+          <div className='btn-container'>
+            <button className='prev-btn' onClick={handlePrev}>
+              prev
+            </button>
+            {data.map((_, index) => {
+              return (
+                <button
+                  className={`page-btn ${
+                    index === currentPage && 'active-btn'
+                  }`}
+                  key={index}
+                  onClick={() => setCurrentPage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className='next-btn' onClick={handleNext}>
+              next
+            </button>
+          </div>
+        )}
       </section>
     </main>
   );
